801bac0c441ef3053c2bc9449561d8053afc0efa
flada-auxv <aseknock@gmail.com>
2016-07-29 00:33:24 +0900

    wip

--- lib/embulk/parser/tenhou_log.rb
+++ lib/embulk/parser/tenhou_log.rb
@@ -5,40 +5,59 @@ module Embulk
       Plugin.register_parser("tenhou_log", self)
 
       def self.transaction(config, &control)
-        # configuration code:
+        decoder_task = config.load_config(Java::LineDecoder::DecoderTask)
+
         task = {
-          "option1" => config.param("option1", :integer),                     # integer, required
-          "option2" => config.param("option2", :string, default: "myvalue"),  # string, optional
-          "option3" => config.param("option3", :string, default: nil),        # string, optional
+          "decoder" => DataSource.from_java(decoder_task.dump)
         }
 
         columns = [
-          Column.new(0, "example", :string),
-          Column.new(1, "column", :long),
-          Column.new(2, "name", :double),
+          Column.new(0, "time", :string),
+          Column.new(1, "game_time", :string),
+          Column.new(2, "player_count", :string),
+          Column.new(3, "grade", :string),
+          Column.new(4, "game_length", :string),
+          Column.new(5, "kuitan", :string),
+          Column.new(6, "aka", :string),
+          Column.new(7, "player1", :string),
+          Column.new(8, "player2", :string),
+          Column.new(9, "player3", :string),
+          Column.new(10, "player4", :string),
         ]
 
         yield(task, columns)
       end
 
       def init
-        # initialization code:
-        @option1 = task["option1"]
-        @option2 = task["option2"]
-        @option3 = task["option3"]
+        @decoder = task.param("decoder", :hash).load_task(Java::LineDecoder::DecoderTask)
       end
 
       def run(file_input)
-        while file = file_input.next_file
-          file.each do |buffer|
-            # parsering code
-            record = ["col1", 2, 3.0]
-            page_builder.add(record)
+        decoder = Java::LineDecoder.new(file_input.to_java, @decoder)
+
+        while decoder.nextFile
+          while line = decoder.poll
+            page_builder.add(parse(line))
           end
         end
         page_builder.finish
       end
-    end
 
+      private
+
+      def parse(line)
+        time, game_time, category, players = line.split(" | ")
+
+        [time, game_time, parse_category(category), parse_players(players)].flatten
+      end
+
+      def parse_category(str)
+        str.chars[0..4]
+      end
+
+      def parse_players(str)
+        str.split
+      end
+    end
   end
 end
